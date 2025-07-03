ca9fa1a943b90e7e0a98a35931f48038de29d323
Muhammad Semeer <muhammadsemeerali@gmail.com>
2025-07-03 15:13:21 +0530

    fix issues on Mac

--- install.sh
+++ install.sh
@@ -132,7 +132,7 @@ fi
 
 # === Download CLI script ===
 echo "[⬇️] Installing ssm-connect CLI to $SCRIPT_PATH"
-curl -fsSL "$SCRIPT_URL" -o "$SCRIPT_PATH"
+sudo curl -fsSL "$SCRIPT_URL" -o "$SCRIPT_PATH"
 chmod +x "$SCRIPT_PATH"
 # === Version file setup ===
 VERSION_FILE="$ALIAS_DIR/version"
